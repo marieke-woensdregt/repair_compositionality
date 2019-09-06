9b42bfe1651aafaa3079423e18992e6fbee21b1d
Marieke Woensdregt <U968195@ip-145-116-141-110.wlan-int.ru.nl>
2019-09-06 14:40:07 +0200

    trying out different way of measuring proportion

--- evolution_compositionality_under_noise.py
+++ evolution_compositionality_under_noise.py
@@ -505,7 +505,7 @@ def produce(language, topic, gamma, error):
 
 
 
-def receive(language, utterance):
+def receive_without_repair(language, utterance):
     """
     Takes a language and an utterance, and returns an interpretation of that utterance, following the language
 
@@ -656,11 +656,24 @@ def language_stats(population):
     :return: a list containing the overall average posterior probability assigned to each class of language in the population, where index 0 = degenerate, index 1 = holistic, index 2 = other, and index 3 = compositional
     (this ordering has to correspond to that defined in the classify_language() function!)
     """
-    stats = [0., 0., 0., 0.]  # degenerate, holistic, other, compositional
+    stats_original_method = [0., 0., 0., 0.]  # degenerate, holistic, other, compositional
+    stats_new = np.zeros(4)  # degenerate, holistic, other, compositional
     for p in population:
         for i in range(len(p)):
-            stats[int(class_per_lang[i])] += np.exp(p[i]) / len(population)
-    return stats
+            if proportion_measure == 'posterior':
+                stats_original_method[int(class_per_lang[i])] += np.exp(p[i]) / len(population)
+                stats_new[int(class_per_lang[i])] += np.exp(p[i])
+            elif proportion_measure == 'sampled':
+                pass
+    print("stats_original_method is:")
+    print(stats_original_method)
+    stats_new_no_axis = np.divide(stats_new, len(population))
+    print("stats_new_no_axis is:")
+    print(stats_new_no_axis)
+    stats_new_axis_0 = np.divide(stats_new, len(population), axis=0)
+    print("stats_new_axis_0 is:")
+    print(stats_new_axis_0)
+    return stats_new_axis_0
 
 
 
@@ -789,13 +802,16 @@ initial = [('02', 'aa'), ('03', 'ab'), ('12', 'bb'), ('13', 'ba')]  # this is da
 gamma = 2  # parameter that determines strength of ambiguity penalty (Kirby et al., 2015 used gamma = 0 for "Learnability Only" condition, and gamma = 2 for both "Expressivity Only" and "Learnability and Expressivity" conditions
 turnover = True  # determines whether new individuals enter the population or not
 b = 20  # the bottleneck (i.e. number of meaning-form pairs the each pair gets to see during training (Kirby et al. used a bottleneck of 20 in the body of the paper.
-rounds = 1*b  # Kirby et al. (2015) used rounds = 2*b, but SimLang lab 21 uses 1*b
+rounds = 2*b  # Kirby et al. (2015) used rounds = 2*b, but SimLang lab 21 uses 1*b
 popsize = 2  # If I understand it correctly, Kirby et al. (2015) used a population size of 2: each generation is simply a pair of agents.
 runs = 50  # the number of independent simulation runs (Kirby et al., 2015 used 100)
-gens = 150  # the number of generations (Kirby et al., 2015 used 100)
-noise = True  # parameter that determines whether environmental noise is on or off
+gens = 800  # the number of generations (Kirby et al., 2015 used 100)
+noise = False  # parameter that determines whether environmental noise is on or off
 noise_prob = 0.1  # the probability of environmental noise masking part of an utterance
-
+proportion_measure = 'posterior'  # the way in which the proportion of language classes present in the population is
+# measured. Can be set to either 'posterior' (where we directly measure the total amount of posterior probability
+# assigned to each language class), or 'sampled' (where at each generation we make all agents in the population pick a
+# language and we count the resulting proportions.
 
 t0 = time.clock()
 
