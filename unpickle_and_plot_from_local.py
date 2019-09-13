6ac3e5231deab29e9cd2b86b25877f3825b62666
Marieke Woensdregt <U968195@MacBook-Pro-van-ISC.local>
2019-09-13 09:31:12 +0200

    added option of multiple batches of runs

--- unpickle_and_plot_from_local.py
+++ unpickle_and_plot_from_local.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from evolution_compositionality_under_noise import plot_timecourse, plot_barplot, classify_all_languages, create_all_possible_languages, dataframe_to_results
+from evolution_compositionality_under_noise import plot_timecourse, plot_barplot, classify_all_languages, create_all_possible_languages, dataframe_to_results, results_to_dataframe
+
 
 
 
@@ -17,14 +18,14 @@ turnover = True  # determines whether new individuals enter the population or no
 b = 20  # the bottleneck (i.e. number of meaning-form pairs the each pair gets to see during training (Kirby et al.
         # used a bottleneck of 20 in the body of the paper.
 rounds = 2*b  # Kirby et al. (2015) used rounds = 2*b, but SimLang lab 21 uses 1*b
-popsize = 2  # If I understand it correctly, Kirby et al. (2015) used a population size of 2: each generation is simply
+popsize = 10  # If I understand it correctly, Kirby et al. (2015) used a population size of 2: each generation is simply
             # a pair of agents.
-runs = 50  # the number of independent simulation runs (Kirby et al., 2015 used 100)
+runs = 20  # the number of independent simulation runs (Kirby et al., 2015 used 100)
 generations = 100  # the number of generations (Kirby et al., 2015 used 100)
 initial_language_type = 'degenerate'  # set the language class that the first generation is trained on
 
 noise = True  # parameter that determines whether environmental noise is on or off
-noise_prob = 0.3  # the probability of environmental noise masking part of an utterance
+noise_prob = 0.9  # the probability of environmental noise masking part of an utterance
 # proportion_measure = 'posterior'  # the way in which the proportion of language classes present in the population is
 # measured. Can be set to either 'posterior' (where we directly measure the total amount of posterior probability
 # assigned to each language class), or 'sampled' (where at each generation we make all agents in the population pick a
@@ -40,7 +41,7 @@ else:
     # "Learnability Only" condition, and gamma = 2 for both "Expressivity Only", and "Learnability and Expressivity"
     # conditions
 minimal_effort = True
-cost_vector = [0.0, 0.2, 0.4]  # costs of no repair, restricted request, and open request, respectively
+cost_vector = [0.0, 0.15, 0.45]  # costs of no repair, restricted request, and open request, respectively
 compressibility_bias = False  # determines whether agents have a prior that favours compressibility, or a flat prior
 observed_meaning = 'intended'  # determines which meaning the learner observes when receiving a meaning-form pair; can
 # be set to either 'intended', where the learner has direct access to the speaker's intended meaning, or 'inferred',
@@ -48,14 +49,48 @@ observed_meaning = 'intended'  # determines which meaning the learner observes w
 
 gen_start = int(generations/2)
 
+batches = 3
 
 
-pickle_file_title = "Pickle_r_" + str(runs) +"_g_" + str(generations) + "_b_" + str(b) + "_rounds_" + str(rounds) + "_pop_size_" + "_mutual_u_"+str(mutual_understanding)+ "_gamma_" + str(gamma) +"_minimal_e_"+str(minimal_effort)+ "_c_"+str(cost_vector)+ "_turnover_" + str(turnover) + "_bias_" +str(compressibility_bias) + "_init_" + initial_language_type + "_noise_" + str(noise) + "_noise_prob_" + str(noise_prob)+"_"+production
+if batches > 1:
+    all_results = []
+    for i in range(batches):
+        pickle_file_title = "Pickle_r_" + str(runs) +"_g_" + str(generations) + "_b_" + str(b) + "_rounds_" + str(rounds) + "_pop_size_" + "_mutual_u_"+str(mutual_understanding)+ "_gamma_" + str(gamma) +"_minimal_e_"+str(minimal_effort)+ "_c_"+str(cost_vector)+ "_turnover_" + str(turnover) + "_bias_" +str(compressibility_bias) + "_init_" + initial_language_type + "_noise_" + str(noise) + "_noise_prob_" + str(noise_prob)+"_"+production+"_"+str(i)
 
-lang_class_prop_over_gen_df = pd.read_pickle(pickle_file_title+".pkl")
+        lang_class_prop_over_gen_df = pd.read_pickle(pickle_file_title+".pkl")
+
+        results = dataframe_to_results(lang_class_prop_over_gen_df)
+
+        for j in range(len(results)):
+            all_results.append(results[j])
+
+    print('')
+    print("len(all_results) are:")
+    print(len(all_results))
+    print("len(all_results[0]) are:")
+    print(len(all_results[0]))
+    print("len(all_results[0][0]) are:")
+    print(len(all_results[0][0]))
+
+    lang_class_prop_over_gen_df = results_to_dataframe(all_results, runs * batches, generations)
+    print('')
+    print('')
+    print("lang_class_prop_over_gen_df is:")
+    print(lang_class_prop_over_gen_df)
 
 
-fig_file_title = "r_" + str(runs) +"_g_" + str(generations) + "_b_" + str(b) + "_rounds_" + str(rounds) + "_pop_size_" + "_mutual_u_"+str(mutual_understanding)+  "_gamma_" + str(gamma) +"_minimal_e_"+str(minimal_effort)+ "_c_"+str(cost_vector)+ "_turnover_" + str(turnover) + "_bias_" +str(compressibility_bias) + "_init_" + initial_language_type + "_noise_" + str(noise) + "_noise_prob_" + str(noise_prob)+"_"+production
+
+else:
+    pickle_file_title = "Pickle_r_" + str(runs) + "_g_" + str(generations) + "_b_" + str(b) + "_rounds_" + str(
+        rounds) + "_pop_size_" + "_mutual_u_" + str(mutual_understanding) + "_gamma_" + str(
+        gamma) + "_minimal_e_" + str(minimal_effort) + "_c_" + str(cost_vector) + "_turnover_" + str(
+        turnover) + "_bias_" + str(compressibility_bias) + "_init_" + initial_language_type + "_noise_" + str(
+        noise) + "_noise_prob_" + str(noise_prob) + "_" + production
+
+    lang_class_prop_over_gen_df = pd.read_pickle(pickle_file_title+".pkl")
+
+
+fig_file_title = "r_" + str(runs*batches) +"_g_" + str(generations) + "_b_" + str(b) + "_rounds_" + str(rounds) + "_pop_size_" + "_mutual_u_"+str(mutual_understanding)+  "_gamma_" + str(gamma) +"_minimal_e_"+str(minimal_effort)+ "_c_"+str(cost_vector)+ "_turnover_" + str(turnover) + "_bias_" +str(compressibility_bias) + "_init_" + initial_language_type + "_noise_" + str(noise) + "_noise_prob_" + str(noise_prob)+"_"+production
 
 if mutual_understanding == False and minimal_effort == False:
     if gamma == 0 and turnover == True:
@@ -95,6 +130,6 @@ print('')
 print("baseline_proportions are:")
 print(baseline_proportions)
 
-plot_barplot(lang_class_prop_over_gen_df, plot_title, fig_file_title, runs, generations, gen_start,
+plot_barplot(lang_class_prop_over_gen_df, plot_title, fig_file_title, runs*batches, generations, gen_start,
              baseline_proportions)
 
