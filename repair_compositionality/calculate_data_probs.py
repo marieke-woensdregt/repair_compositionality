108f1974fa4d3b362804e707f0f95348f9ac8b05
Marieke Woensdregt <U968195@ip-145-116-143-186.wlan-int.ru.nl>
2019-10-16 13:35:40 +0200

    .

---
+++ repair_compositionality/calculate_data_probs.py
@@ -0,0 +1,50 @@
+import numpy as np
+from evolution_compositionality_under_noise import create_all_possible_languages, classify_all_languages
+
+
+
+###################################################################################################################
+# ALL PARAMETER SETTINGS GO HERE:
+
+meanings = ['02', '03', '12', '13']  # all possible meanings
+forms_without_noise = ['aa', 'ab', 'ba', 'bb']  # all possible forms, excluding their possible 'noisy variants'
+noisy_forms = ['a_', 'b_', '_a', '_b']  # all possible noisy variants of the forms above
+all_forms_including_noisy_variants = forms_without_noise+noisy_forms  # all possible forms, including both complete
+# forms and noisy variants
+error = 0.05  # the probability of making a production error (Kirby et al., 2015 use 0.05)
+
+turnover = True  # determines whether new individuals enter the population or not
+b = 20  # the bottleneck (i.e. number of meaning-form pairs the each pair gets to see during training (Kirby et al.
+        # used a bottleneck of 20 in the body of the paper.
+rounds = 2*b  # Kirby et al. (2015) used rounds = 2*b, but SimLang lab 21 uses 1*b
+popsize = 2  # If I understand it correctly, Kirby et al. (2015) used a population size of 2: each generation is simply
+            # a pair of agents.
+runs = 100  # the number of independent simulation runs (Kirby et al., 2015 used 100)
+generations = 150  # the number of generations (Kirby et al., 2015 used 100)
+initial_language_type = 'degenerate'  # set the language class that the first generation is trained on
+
+production = 'my_code'  # can be set to 'simlang' or 'my_code'
+
+cost_vector = np.array([0.0, 0.2, 0.4])  # costs of no repair, restricted request, and open request, respectively
+
+
+
+hypothesis_space = create_all_possible_languages(meanings, forms_without_noise)
+print("number of possible languages is:")
+print(len(hypothesis_space))
+
+class_per_lang = classify_all_languages(hypothesis_space, forms_without_noise, meanings)
+print('')
+print('')
+print("class_per_lang is:")
+print(class_per_lang)
+no_of_each_class = np.bincount(class_per_lang.astype(int))
+print('')
+print("no_of_each_class ACCORDING TO MY CODE, where 0 = degenerate, 1 = holistic, 2 = hybrid, 3 = compositional, "
+      "4 = other is:")
+print(no_of_each_class)
+
+
+def calc_prob_of_repair(language, costs, prob_of_noise):
+    for
+
