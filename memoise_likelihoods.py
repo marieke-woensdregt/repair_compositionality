ece068fc7686b1c2483baa7c02d33c2e74f8cd51
marieke-woensdregt <marieke.woe@gmail.com>
2020-01-13 21:29:12 +0100

    added parameters to pickle filename

--- memoise_likelihoods.py
+++ memoise_likelihoods.py
@@ -1,5 +1,6 @@
 from evolution_compositionality_under_noise import *
 import numpy as np
+import time
 import pickle
 
 
@@ -54,93 +55,17 @@ noise_prob = 0.5  # the probability of environmental noise obscuring part of an 
 # FUNCTION DEFINITIONS:
 
 
-def enumerate_all_possible_datapoints(meaning_list, all_possible_forms):
-    """
-    Enumerates all possible <meaning, form> pairs and saves them as tuples in a list
 
-    :param meaning_list: list of strings corresponding to all possible meanings
-    :param all_possible_forms: list of strings corresponding to all possible forms, including their noisy variants
-    :return: list of <meaning, form> tuples
-    """
-    mf_pair_list = []
-    for m in meaning_list:
-        for f in all_possible_forms:
-            mf_pair = (m, f)
-            mf_pair_list.append(mf_pair)
-    return mf_pair_list
-
-
-
-def cache_likelihoods_per_datapoint(all_datapoints, hypotheses):
-
-    likelihood_cache_matrix = np.zeros((len(all_datapoints), len(hypotheses)))
-    print("likelihood_cache_matrix.shape is:")
-    print(likelihood_cache_matrix.shape)
-    for d in range(len(all_datapoints)):
-        counter = 0
-        print('')
-        print("d is:")
-        print(d)
-        topic = all_datapoints[d][0]
-        print("topic is:")
-        print(topic)
-        form = all_datapoints[d][1]
-        print("form is:")
-        print(form)
+def cache_likelihoods_per_datapoint(meaning_list, forms_including_noisy_variants, hypotheses):
+    likelihood_cache_matrix = np.zeros((len(meaning_list), len(forms_including_noisy_variants), len(hypotheses)))
+    for m in range(len(meaning_list)):
+        topic = meaning_list[m]
         for h in range(len(hypotheses)):
-            print('')
-            print("h is:")
-            print(h)
             prop_to_prob_per_form_array = production_likelihoods_with_noise(hypotheses[h], topic, meanings,
                                                                              forms_without_noise, noisy_forms, gamma,
                                                                              error, noise_prob)
-            print("len(prop_to_prob_per_form_array) is:")
-            print(len(prop_to_prob_per_form_array))
-            print("len(all_forms_including_noisy_variants) is:")
-            print(len(all_forms_including_noisy_variants))
-            print("range(d, d+len(prop_to_prob_per_form_array)) is:")
-            print(range(d, d+len(prop_to_prob_per_form_array)))
-            for i in range(d, d+len(prop_to_prob_per_form_array)):
-                print('')
-                print("i is:")
-                print(i)
-                likelihood_cache_matrix[i][h] = prop_to_prob_per_form_array[counter]
-                counter += 1
-
-    return likelihood_cache_matrix
-
-
-
-def cache_likelihoods_per_datapoint_new(meaning_list, forms_including_noisy_variants, hypotheses):
-    print('')
-    print('')
-    print("This is the cache_likelihoods_per_datapoint_new() function:")
-    likelihood_cache_matrix = np.zeros((len(meaning_list), len(forms_including_noisy_variants), len(hypotheses)))
-    print("likelihood_cache_matrix.shape is:")
-    print(likelihood_cache_matrix.shape)
-    for m in range(len(meaning_list)):
-        for f in range(len(forms_including_noisy_variants)):
-            print('')
-            topic = meaning_list[m]
-            print("topic is:")
-            print(topic)
-            form = forms_including_noisy_variants[f]
-            print("form is:")
-            print(form)
-            for h in range(len(hypotheses)):
-                print('')
-                print("h is:")
-                print(h)
-                prop_to_prob_per_form_array = production_likelihoods_with_noise(hypotheses[h], topic, meanings,
-                                                                                 forms_without_noise, noisy_forms, gamma,
-                                                                                 error, noise_prob)
-                print("len(prop_to_prob_per_form_array) is:")
-                print(len(prop_to_prob_per_form_array))
-                for i in range(len(prop_to_prob_per_form_array)):
-                    likelihood_cache_matrix[m][i][h] = prop_to_prob_per_form_array[i]
-        print('')
-        print("likelihood_cache_matrix is:")
-        print(likelihood_cache_matrix)
+            for f in range(len(prop_to_prob_per_form_array)):
+                likelihood_cache_matrix[m][f][h] = prop_to_prob_per_form_array[f]
     return likelihood_cache_matrix
 
 
@@ -148,35 +73,21 @@ def cache_likelihoods_per_datapoint_new(meaning_list, forms_including_noisy_vari
 ###################################################################################################################
 # MEMOISING LIKELIHOODS FOR ALL POSSIBLE <MEANING, FORM> COMBINATIONS AND SAVING RESULTING MATRIX AS PICKLE FILE:
 
+t0 = time.process_time()
 
-all_possible_datapoints = enumerate_all_possible_datapoints(meanings, all_forms_including_noisy_variants)
+likelihood_cache = cache_likelihoods_per_datapoint(meanings, all_forms_including_noisy_variants, hypothesis_space)
+
+t1 = time.process_time()
+
 print('')
 print('')
-print("all_possible_datapoints is:")
-print(all_possible_datapoints)
-print("len(all_possible_datapoints) is:")
-print(len(all_possible_datapoints))
+print('min. it took to fill out likelihood cache:')
+print((t1-t0)/60)
 
-#
-# likelihood_cache = cache_likelihoods_per_datapoint(all_possible_datapoints, hypothesis_space[0:10])
-# print('')
-# print('')
+print('')
+print('')
 # print("likelihood_cache is:")
 # print(likelihood_cache)
-# print('')
-# print('')
-# print("np.exp(likelihood_cache) is:")
-# print(np.exp(likelihood_cache))
-# print("likelihood_cache.shape is:")
-# print(likelihood_cache.shape)
-
-
-
-likelihood_cache = cache_likelihoods_per_datapoint_new(meanings, all_forms_including_noisy_variants, hypothesis_space[0:6])
-print('')
-print('')
-print("likelihood_cache is:")
-print(likelihood_cache)
 print('')
 print('')
 print("np.exp(likelihood_cache) is:")
@@ -184,3 +95,13 @@ print(np.exp(likelihood_cache))
 print("likelihood_cache.shape is:")
 print(likelihood_cache.shape)
 
+
+t2 = time.process_time()
+
+pickle.dump(likelihood_cache, open("pickles/likelihood_cache_noise_"+str(noise)+"_"+convert_float_value_to_string(noise_prob)+"_gamma_"+str(gamma)+"_error_"+convert_float_value_to_string(error)+".p", "wb"))
+
+t3 = time.process_time()
+print('')
+print('')
+print('min. it took to pickle likelihood cache:')
+print((t3-t2)/60)
