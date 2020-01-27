45af405388384393b25d1caf59fd61bcd8c3690d
marieke-woensdregt <marieke.woe@gmail.com>
2020-01-27 12:00:56 +0100

    changed parameter settings

--- evolution_compositionality_under_noise.py
+++ evolution_compositionality_under_noise.py
@@ -101,9 +101,9 @@ error = 0.05  # the probability of making a production error (Kirby et al., 2015
 turnover = True  # determines whether new individuals enter the population or not
 popsize = 2  # If I understand it correctly, Kirby et al. (2015) used a population size of 2: each generation is simply
 # a pair of agents.
-runs = 100  # the number of independent simulation runs (Kirby et al., 2015 used 100)
-generations = 150  # the number of generations (Kirby et al., 2015 used 100)
-initial_language_type = 'degenerate'  # set the language class that the first generation is trained on
+runs = 10  # the number of independent simulation runs (Kirby et al., 2015 used 100)
+generations = 2000  # the number of generations (Kirby et al., 2015 used 100)
+initial_language_type = 'holistic'  # set the language class that the first generation is trained on
 
 production = 'my_code'  # can be set to 'simlang' or 'my_code'
 
